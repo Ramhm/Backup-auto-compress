5b18db777fc50a333a2803f5f6644f10d49a9903
Ramin Hakimi <38656846+Ramhm@users.noreply.github.com>
2018-11-13 11:41:40 +0330

    Create auto-compress.sh

---
+++ auto-compress.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+###R.HAKIMI
+SRCDIR="/tmp"
+DATE=$(date +%Y%m%d-%H%M%S)
+
+
+while true
+do
+
+	find $SRCDIR -type f -name "*.bak" -print > Filelist.log
+	echo "DONE." >> Filelist.log
+	FILE=$(head -n1 Filelist.log)
+        echo $DATE URL:$FILE > File_Name.log
+
+	if [ "$FILE" == "DONE." ];then
+        echo $DATE "Empty File" > Start_End.log
+        exit 1
+else
+        tar -I pigz -cf $FILE.tgz -P $FILE
+	echo $DATE "Compress File" >> Start_End.log
+        rm -rf $FILE
+fi
+done
